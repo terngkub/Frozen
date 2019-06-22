0438dae66d4fdcd21706e5dec2e11b83fa35699e
eparisot <eparisot@student.42.fr>
2019-06-22 18:30:57 +0200

    pvtmsg start

---
+++ authorize.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func (session *Session) authorize() error {
+	request := session.getRequest()
+	matches := doRegexpSubmatch("PASS (.*)\r\nNICK (.*)\r\nUSER (.*) .* .* :.*\r\n", request)
+	if len(matches) != 4 {
+		return errors.New("wrong request format")
+	}
+	pass := "test" //matches[1]
+	nick := "klh"  //matches[2]
+	user := "eric" //matches[3]
+
+	account, ok := session.Env.UserList[user]
+	if ok {
+		// check password
+		if pass != account.Password {
+			return errors.New("wrong password")
+		}
+	} else {
+		// create new user
+		newAccount := Account{Password: pass, Nickname: nick, User: user}
+		session.Env.AccountList = append(session.Env.AccountList, newAccount)
+		session.Account = &session.Env.AccountList[len(session.Env.AccountList)-1]
+		session.Env.UserList[user] = &session.Env.AccountList[len(session.Env.AccountList)-1]
+	}
+	session.Env.ConnList[nick] = session.Conn
+	fmt.Println("correct")
+	message := fmt.Sprintf(":%s 001 %s :Welcome to the Internet Relay Network %s!%s@%s", "127.0.0.1", nick, nick, user, "127.0.0.1")
+	fmt.Println(message)
+	session.Conn.Write([]byte(message))
+
+	return nil
+}
--- simple_server.go
+++ simple_server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 const (
@@ -19,8 +20,17 @@ type Account struct {
 	Nickname string
 }
 
+type Channel struct {
+	Name string
+	Topic string
+	UserList []*Account
+}
+
 type Env struct {
 	AccountList []Account
+	UserList map[string]*Account
+	ConnList map[string]net.Conn
+	ChannelList []Channel
 }
 
 type Session struct {
@@ -65,6 +75,7 @@ func runSession(env *Env, conn net.Conn) {
 
 func (session *Session) handleRequest(request string) {
 	// handle request here
+	//privateMSG(session)
 }
 
 func (session *Session) getRequest() string {
@@ -74,18 +85,38 @@ func (session *Session) getRequest() string {
 		log.Println("Error reading: ", err)
 	}
 	requestStr := string(request[:len])
-	fmt.Print(requestStr)
 	return requestStr
-}
-
-func (session *Session) authorize() {
-	session.cmdPASS()
-	// NICK
-	// USER
 }
 
 func (session *Session) cmdPASS() {
 	request := session.getRequest()
 	matches := doRegexpSubmatch("PASS (.*)\r\n", request)
 	fmt.Println(matches)
+}
+
+func privateMSG(session *Session) {
+	request := session.getRequest()
+	matches := doRegexpSubmatch("PRIVMSG (.*)\r\n", request)
+	//if dst exists
+	var dst string
+	if len(matches) > 0 {
+		for _, usr := range session.Env.AccountList {
+			if matches[1] == usr.Nickname {
+				dst = usr.Nickname
+				fmt.Println(dst)
+			}
+		}
+	}
+	//grab message
+
+	if len(request) > 1 {
+		i := strings.Index(request[1:], ":")
+		if i != 0 {
+			msg := request[i+1:]
+			//get dst's connexion
+			dst_conn := session.Env.ConnList[dst]
+			//send message from src to dst
+			dst_conn.Write([]byte(msg))
+		}
+	}
 }
