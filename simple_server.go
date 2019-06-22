2982ca1d7c5d689fd2237245d0f62981925a3767
eparisot <eparisot@student.42.fr>
2019-06-23 00:22:07 +0200

    JOIN : parse channels

--- simple_server.go
+++ simple_server.go
@@ -40,16 +40,16 @@ type Session struct {
 }
 
 func main() {
-
 	ln, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
 		fmt.Println("Error listening :", err.Error())
 		os.Exit(1)
 	}
 	defer ln.Close()
-
-	env := Env{AccountList: []Account{}, UserMap: make(map[string]*Account), NicknameMap: make(map[string]*Account), ConnMap: make(map[string]net.Conn)}
-
+	env := Env{AccountList: []Account{}, 
+							UserMap: make(map[string]*Account), 
+							NicknameMap: make(map[string]*Account), 
+							ConnMap: make(map[string]net.Conn)}
 	fmt.Println("Listening on ", CONN_HOST+":"+CONN_PORT)
 	for {
 		conn, err := ln.Accept()
@@ -66,12 +66,10 @@ func runSession(env *Env, conn net.Conn) {
 	session := Session{Env: env, Conn: conn, Account: nil}
 	defer session.Conn.Close()
 	session.authorize()
-
 	fmt.Println("AccountList", session.Env.AccountList)
 	fmt.Println("UserMap", session.Env.UserMap)
 	fmt.Println("UserMap", session.Env.UserMap)
 	fmt.Println("ConnMap", session.Env.ConnMap)
-
 	for {
 		request, err := session.getRequest()
 		if err != nil {
@@ -85,8 +83,9 @@ func (session *Session) handleRequest(request string) {
 	switch {
 	case strings.Contains(request, "PRIVMSG"):
 		session.privateMSG(request)
+	case strings.Contains(request, "JOIN"):
+		session.joinChan(request)
 	}
-
 }
 
 func (session *Session) getRequest() (string, error) {
@@ -108,12 +107,7 @@ func (session *Session) privateMSG(request string) {
 	//if dst exists
 	var dst_nick string
 	if len(matches) > 0 {
-		for _, usr := range session.Env.AccountList {
-			fmt.Println("dst nick :", usr.Nickname)
-			if matches[1] == usr.Nickname {
-				dst_nick = usr.Nickname
-			}
-		}
+		dst_nick = session.Env.NicknameMap[matches[1]].Nickname
 	}
 	//grab message
 	if len(request) > 1 {
@@ -131,4 +125,27 @@ func (session *Session) privateMSG(request string) {
 			dst_conn.Write([]byte(msg))
 		}
 	}
+}
+
+func (session *Session) joinChan(request string) {
+	//src_user := session.Account.User
+	//matches := doRegexpSubmatch("JOIN (.*) ,(.*)", request)
+	
+	var req_chans []string
+	//var req_keys []string
+	start := 0
+	end := 0
+	request = request[len("JOIN "):]
+	for i, char := range request {
+		if char == '#' || char == '&' {
+			start = i + 1
+		} else if char == ',' || char == ' ' {
+			end = i
+			req_chans = append(req_chans, request[start:end])
+			if char == ' ' {
+				break
+			}
+		}
+	}
+	//fmt.Println(req_chans)
 }
