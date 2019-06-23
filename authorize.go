5587c7f268697a17562316255f2dd8d1167409a3
terngkub <terngnattapol@gmail.com>
2019-06-23 16:42:46 +0200

    refactor code

--- authorize.go
+++ authorize.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"errors"
-	"fmt"
+	"strings"
 )
+
+func (session *Session) authorize() error {
+	newAccount := Account{}
+
+	for !newAccount.isComplete() {
+
+		// TODO parse this in a better manner
+		request, err := session.getRequest()
+		if err != nil {
+			return err
+		}
+
+		switch {
+		case strings.HasPrefix(request, "PASS"):
+			if newPass := session.cmdPASS(request); newPass != "" {
+				newAccount.Password = newPass
+			}
+		case strings.HasPrefix(request, "NICK"):
+			if newNick := session.cmdNICK(request); newNick != "" {
+				newAccount.Nickname = newNick
+			}
+		case strings.HasPrefix(request, "USER"):
+			if newUser := session.cmdUSER(request); newUser != "" {
+				newAccount.User = newUser
+			}
+			// TODO handle unmatch case
+		}
+	}
+
+	account, ok := session.Env.UserMap[newAccount.User]
+	if ok {
+		if newAccount.Password != account.Password {
+			return errors.New("wrong password")
+		}
+	} else {
+		session.register(newAccount)
+	}
+	session.Env.ConnMap[newAccount.Nickname] = session.Conn
+
+	return nil
+}
 
 func (account *Account) isComplete() bool {
 	if account.Password != "" && account.Nickname != "" && account.User != "" {
@@ -12,81 +53,56 @@ func (account *Account) isComplete() bool {
 	return false
 }
 
-func (session *Session) authorize() error {
-	newAccount := Account{}
-
-	for !newAccount.isComplete() {
-
-		request, err := session.getRequest()
-		if err != nil {
-			return err
-		}
-
-		newPass, err := session.cmdPASS(request)
-		if err != nil {
-			continue
-		}
-		if newPass != "" {
-			newAccount.Password = newPass
-		}
-
-		matches := doRegexpSubmatch("NICK +(.+)\r\n", request)
-		if len(matches) == 2 {
-			_, duplicated := session.Env.NicknameMap[matches[1]]
-			if duplicated {
-				message := fmt.Sprintf(":%s 443 * %s :Nickname is already in use.\r\n", "127.0.0.1", matches[1])
-				session.Conn.Write([]byte(message))
-			} else {
-				newAccount.Nickname = matches[1]
-			}
-		}
-
-		matches = doRegexpSubmatch("USER +(.+) +.+ +.+ +:.+\r\n", request)
-		if len(matches) == 2 {
-			newAccount.User = matches[1]
-		}
+func (session *Session) cmdPASS(request string) string {
+	if session.Account != nil {
+		session.error462()
+		return ""
 	}
-
-	account, ok := session.Env.UserMap[newAccount.User]
-	if ok {
-		// check password
-		if newAccount.Password != account.Password {
-			return errors.New("wrong password")
-		}
-	} else {
-		// create new user
-		session.Env.AccountList = append(session.Env.AccountList, newAccount)
-		session.Account = &session.Env.AccountList[len(session.Env.AccountList)-1]
-		session.Env.UserMap[newAccount.User] = &session.Env.AccountList[len(session.Env.AccountList)-1]
-		session.Env.NicknameMap[newAccount.Nickname] = &session.Env.AccountList[len(session.Env.AccountList)-1]
+	matches := doRegexpSubmatch("PASS +(.+)\r\n", request)
+	if len(matches) != 2 {
+		session.error461("PASS")
+		return ""
 	}
-	session.Env.ConnMap[newAccount.Nickname] = session.Conn
-	message := fmt.Sprintf(":%s %s %s %s\r\n", "127.0.0.1", "001", newAccount.Nickname, ":Welcome to the Internet Relay Network")
-	fmt.Println(message)
-	session.Conn.Write([]byte(message))
-
-	return nil
+	return matches[1]
 }
 
-func (session *Session) cmdPASS(request string) (string, error) {
-	if session.Account != nil {
-		message := fmt.Sprintf(":%s 462 :You may not reregister", CONN_HOST)
-		session.Conn.Write([]byte(message))
-		return "", errors.New("462")
-	}
-
-	matches := doRegexpSubmatch("(?:.+ ){0,1}PASS +(.*)\r\n", request)
+func (session *Session) cmdNICK(request string) string {
+	matches := doRegexpSubmatch("NICK +(.+)\r\n", request)
 	if len(matches) != 2 {
-		return "", nil
+		session.error431()
+		return ""
 	}
-
-	if matches[1] == "" {
-		message := fmt.Sprintf(":%s 461 %s :Not enough parameters", CONN_HOST, "PASS")
-		session.Conn.Write([]byte(message))
-		return "", errors.New("461")
+	if !isValidNickname(matches[1]) {
+		session.error432(matches[1])
+		return ""
 	}
+	if _, isDuplicated := session.Env.NicknameMap[matches[1]]; isDuplicated {
+		session.error434(matches[1])
+		return ""
+	}
+	return matches[1]
+}
 
-	return matches[1], nil
+func (session *Session) cmdUSER(request string) string {
+	if session.Account != nil {
+		session.error462()
+		return ""
+	}
+	matches := doRegexpSubmatch("USER +(.+) +.+ +.+ +:.+\r\n", request)
+	if len(matches) != 2 {
+		session.error461("USER")
+		return ""
+	}
+	return matches[1]
+}
+
+func (session *Session) register(account Account) {
+	session.Env.AccountList = append(session.Env.AccountList, account)
+	length := len(session.Env.AccountList)
+	last := &session.Env.AccountList[length-1]
+	session.Account = last
+	session.Env.UserMap[account.User] = last
+	session.Env.NicknameMap[account.Nickname] = last
 }
 
 func (session *Session) changeNickname(request string) {
---
+++ reply.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func (session *Session) reply(message string) {
+	session.Conn.Write([]byte(message))
+	log.Printf("reply: '%s'", message)
+}
+
+func (session *Session) welcome() {
+	message := fmt.Sprintf(":%s 001 %s :Welcome to the Internet Relay Network\r\n", CONN_HOST, session.Account.Nickname)
+	session.reply(message)
+}
+
+// Nickname
+
+func (session *Session) error431() {
+	message := fmt.Sprintf(":%s 431 :No nickname is given\r\n", CONN_HOST)
+	session.reply(message)
+}
+
+func (session *Session) error432(nick string) {
+	message := fmt.Sprintf(":%s 432 %s :Erroneus nickname\r\n", CONN_HOST, nick)
+	session.reply(message)
+}
+
+func (session *Session) error433(nick string) {
+	message := fmt.Sprintf(":%s 433 * %s :Nickname collision KILL\r\n", CONN_HOST, nick)
+	session.reply(message)
+}
+
+func (session *Session) error434(nick string) {
+	message := fmt.Sprintf(":%s 434 * %s :Nickname is already in use\r\n", CONN_HOST, nick)
+	session.reply(message)
+}
+
+// Register
+
+func (session *Session) error461(command string) {
+	message := fmt.Sprintf(":%s 461 %s :Not enough parameters\r\n", CONN_HOST, command)
+	session.reply(message)
+}
+
+func (session *Session) error462() {
+	message := fmt.Sprintf(":%s 462 :You may not reregister\r\n", CONN_HOST)
+	session.reply(message)
+}
