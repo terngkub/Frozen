4e8b9efef892f31f75b2804364ce692f4065aa4b
eparisot <eparisot@student.42.fr>
2019-06-25 14:53:02 +0200

    refresh rendu

--- utils.go
+++ utils.go
@@ -9,6 +9,9 @@ func doRegexpSubmatch(format, str string) []string {
 }
 
 func remove(s []*Account, i int) []*Account {
+	if len(s) == 1 {
+		return nil
+	}
 	s[len(s)-1], s[i] = s[i], s[len(s)-1]
 	return s[:len(s)-1]
 }
